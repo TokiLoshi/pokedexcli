dad92bca79b53ad53b6f4e765f9aa5a8c29e460f
TokiLoshi <103384402+TokiLoshi@users.noreply.github.com>
2024-09-11 16:55:45 -0700

    improving print statements

--- command_catch.go
+++ command_catch.go
@@ -21,12 +21,13 @@ func commandCatch(cfg *config, args ...string) error {
 	randomNumber := rand.Intn(10) * experience / 10
 
 	userGeneratedNumber := rand.Intn(100)
-	
+	fmt.Printf("Throwing a pokeball at %v\n", pokemonCatch.Name)
 	// Evaluate if user caught the pokemon
 	if randomNumber > userGeneratedNumber {
 		fmt.Printf("%v escaped!\n", pokemonCatch.Name)
 	} else {
-		fmt.Printf("congratulations you caught: %v\n", pokemonCatch.Name)
+		fmt.Printf("%v was caught!\n", pokemonCatch.Name)
+		fmt.Printf("You may now inspect it with the inspect command\n")
 	}
 
 	cfg.caughtPokemon[pokemonCatch.Name] = pokemonCatch
